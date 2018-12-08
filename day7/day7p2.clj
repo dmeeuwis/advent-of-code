ecee3b7807bc367f230035e09afd512a823ac2f3
David Meeuwis <dmeeuwis@gmail.com>
2018-12-07 21:11:43 -0800

    Finish day 7 part 2

--- day7/day7p2.clj
+++ day7/day7p2.clj
@@ -26,9 +26,9 @@
                coll
                (conj coll (first k)))))))
 
-(defn find-finished-job [time busy-worker]
+(defn find-finished-job [min-time time busy-worker]
   (let [job-time (inc (- (int (.charAt (busy-worker :step) 0)) 65))]
-    (if (>= time (+ (busy-worker :start) job-time))
+    (if (>= time (+ (busy-worker :start) job-time min-time))
       busy-worker
       nil)))
 
@@ -57,30 +57,34 @@
     (= target (first v)) true
     :default (recur (rest v) target)))
 
-(defn order-steps [all-claims all-steps workers]
+(defn order-steps [min-time all-claims all-steps workers]
   (loop [claims all-claims, steps all-steps, work-in-progress [], order "", time 0]
     (println "------------------------------------------------")
     (println time order steps work-in-progress)
     (println claims)
     (if (and (empty? steps) (empty? work-in-progress))
-      [order time]
+      [order (dec time)]
 
-      (let [finished-workers (filter #(find-finished-job time %) work-in-progress)
+      (let [finished-workers (filter #(find-finished-job min-time time %) work-in-progress)
             just-completed-steps (map :step finished-workers)
             still-busy-workers (vector-rm work-in-progress finished-workers)
 
-            mapped (preceed-maps claims)
+            adjusted-claims (filter #(not (in just-completed-steps (first %))) claims)
+
+            mapped (preceed-maps adjusted-claims)
             ready-steps (sort (find-not-in-map steps mapped))
 
-            available-workers (vector-rm workers still-busy-workers)
+            available-workers (vector-rm workers (map :worker still-busy-workers))
             new-workers (assign available-workers ready-steps time)
 
             now-busy-workers (concat still-busy-workers new-workers)
            ]
+        (println "still-busy-workers" still-busy-workers)
+        (println "available workers" available-workers)
         (println "Started:" new-workers)
         (println "Finished" finished-workers)
 
-        (recur (filter #(not (in just-completed-steps (first %))) claims) 
+        (recur adjusted-claims 
                (vector-rm steps (map :step new-workers))
                now-busy-workers
                (str order (string/join "" just-completed-steps))
@@ -91,6 +95,7 @@
                      (string/split #"\n")
                      (->> (map read-dep-line)))]
 
-  (println (order-steps all-claims 
+  (println (order-steps 60
+                        all-claims 
                         (set (flatten all-claims))
-                        [ :w1 :w2 ])))
+                        [ :w1 :w2 :w3 :w4 :w5 ])))
