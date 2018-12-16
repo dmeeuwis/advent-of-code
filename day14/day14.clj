6f0616ad708b343fb62090c698e6f285c91004ea
David Meeuwis <dmeeuwis@gmail.com>
2018-12-15 21:09:11 -0800

    Initial soln, day 14 p1

--- day14/day14.clj
+++ day14/day14.clj
@@ -1,25 +1,28 @@
-(def input 190221)
-(def start '(3 7))
+(def init-board '(3 7))
+(def init-elfs '( { :index 0 :value 3 } {:index 1 :value 7 }))
 
 (defn combine-recipes [recipes]
   (let [s (apply + recipes)]
-    (seq (str s))))
+    (map #(Integer/parseInt (str %)) (seq (str s)))))
 
 (defn move-elf [elf board]
-  (let [moved
-    (let [index (mod (+ (elf :index) (elf :value) 1) (count board))]
-      { :index index
-        :value (nth board index) })]
+  (let [index (mod (+ (elf :index) (elf :value) 1) (count board))]
+    { :index index, :value (nth board index) }))
 
-    (println "Moved from" elf "to" moved)
-    moved))
+(defn step [board elfs]
+  (let [new (concat board (combine-recipes (map :value elfs)))]
+    { :board new, :elfs (doall (map #(move-elf % new) elfs)) }))
 
-(defn step [board elves]
-  (let [new (concat board (combine-recipes (map :value elves)))]
-    { :board new
-      :elves (doall (map #(move-elf % new) elves)) }))
+(defn run-for [input]
+  (println "For" input "result is" 
+    (loop [board init-board, elfs init-elfs, i 0 ]
+      (if (<= (+ 10 input) (count board))
+        (clojure.string/join (take-last 10 (take (+ 10 input) board)))
+        (let [step-out (step board elfs)]
+          (recur (:board step-out) (:elfs step-out) (inc i)))))))
 
-(println 
-  (->> '( { :index 0 :value 3 } {:index 1 :value 7 })
-    #(step start)
-    #(step start)))
+(run-for 9)
+(run-for 5)
+(run-for 18)
+(run-for 2018)
+(run-for 190221)
