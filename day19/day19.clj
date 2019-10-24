1a5e93909fff5b96ddf879d939d92b98c50bb2fd
David Meeuwis <dmeeuwis@gmail.com>
2019-10-23 22:10:02 -0700

    Day 19 part 1 soln

--- day19/day19.clj
+++ day19/day19.clj
@@ -1,4 +1,5 @@
-(use 'com.rpl.specter)
+(ns dmeeuwis.advent2018 
+  [:require [clojure.string :as str]])
 
 (defn addr [R a b c] (assoc R c (+ (R a) (R b)))) 
 (defn addi [R a b c] (assoc R c (+ (R a) b))) 
@@ -22,42 +23,54 @@
 
 (defn read-input [lines]
   (loop [coll [], l lines]
-    (if (empty? l) 
+    (if (empty? l)
       coll
-      (let [instruction (re-matches #"(\s+) (\d+) (\d+) (\d+)$" (nth l 1))
-            ip-change   (re-matches #"#ip (\d)")]
+      (let [instruction (re-matches #"(\w+) (\d+) (\d+) (\d+)$" (first l))
+            instruction-parsed (if instruction [(nth instruction 1) (Integer/parseInt (nth instruction 2))
+                                                                    (Integer/parseInt (nth instruction 3))
+                                                                    (Integer/parseInt (nth instruction 4))]
+                                               nil)
+            ip-change   (re-matches #"(#ip) (\d)" (first l))
+            ip-change-parsed (if ip-change [(nth ip-change 1) (Integer/parseInt (nth ip-change 2))])]
+
         (recur (conj coll
-                  (or instruction ip-change))
+                  (vec (or instruction-parsed ip-change-parsed)))
                (rest l))))))
 
-(defn exec [R ip ip-binding op]
+(defn exec [R ip op]
   (let [command (first op)
+        _ (print (str "ip=" ip " " (pr-str R) " " (str/join " " op) " "))
        result (apply (resolve (symbol command)) R (rest op))]
-    (println (str "ip=" ip " [" (str/join ", " R) "] " (str/join op " ") " [" (str/join result) "]"))
+    (println (pr-str result))
     result))
 
-(defn run-program [program]
-  (loop [R [0 0 0 0 0 0], ip 0, p program]
-    (cond
-      (empty? p)
-      R
+(defn run-program 
+  ([program]
+    (run-program [0 0 0 0 0 0], 0, nil, program))
 
-      (= "#ip" (first p))
-      ; special command to bind ip register
-      (recur R ip 
-                (second (first p))
-                (rest p)))
+   ([R ip ip-binding program]
+      (cond
+        (>= ip (count program))
+        R
 
-         ; normal command
-      (let [R2 (if ip-bound (copy-ip-in R ip-bound) R)
-            result (exec R ip (first p))]
-         (let [ip-new (if ip-bound (copy-ip-out R ip-bound) ip-bound)]
-           (recur R2 ip-new ip-bound 
-                  (rest p))))))
+        (= "#ip" (first (nth program ip)))
+        ; special command to bind ip register
+        (do (println "Binding ip to" (second (nth program ip)))
+            (run-program R ip 
+                   (second (nth program ip))
+                   (rest program)))
+
+        ; normal command
+        :default
+        (let [R2 (if ip-binding (assoc R ip-binding ip) R)
+              result (exec R2 ip (nth program ip))]
+           (let [ip-new (if ip-binding (get result ip-binding)  ip)]
+             (recur result (inc ip-new) ip-binding 
+                    program))))))
 
 (let [program (-> (first *command-line-args*)
               slurp
               (clojure.string/split #"\n") 
               read-input)]
-  (println "Program read as" program)
+  (println "Program read as" (pr-str program))
   (println "Final state:" (run-program program)))
