d2a4a44565b1fbafa8ba7d8179a03e1d8861af70
David Meeuwis <dmeeuwis@gmail.com>
2023-07-10 17:11:58 -0700

    Day 23 part 1

--- 2018/day23/day23.clj
+++ 2018/day23/day23.clj
@@ -1,10 +1,36 @@
 (ns dmeeuwis.advent2018
   (:require [clojure.string :as string]))
 
+(defn inte [n] 
+  (Integer/parseInt n))
+
+(defn square [n]
+  (* n n))
+
+(defn taxicab_e [p1 p2]
+  (Math/sqrt (+
+         (square (- (first p1) (first p2)))
+         (square (- (second p1) (second p2)))
+         (square (- (nth p1 2) (nth p2 2))))))
+
+
+(defn taxicab [p1 p2]
+  (+
+     (abs (- (first p1) (first p2)))
+     (abs (- (second p1) (second p2)))
+     (abs (- (nth p1 2) (nth p2 2)))))
 
 (let [lines (-> (first *command-line-args*) (slurp) (string/split #"\n"))
-      pattern #"pos=<(\d+),(\d+),(\d+)>, r=(\d+)"
+      pattern #"pos=<(-?\d+),(-?\d+),(-?\d+)>, r=(-?\d+)"
       nb-matches (map #(re-matches pattern %) lines)
-      nanobots (doall (map #( [ [(nth % 1) (nth % 2) (nth % 3)] (nth % 4) ]) nb-matches))
+      nanobots (map #(list [(inte (nth % 1)) (inte (nth % 2)) (inte (nth % 3))] (inte (nth % 4))) nb-matches)
+
+      sorted (sort-by second nanobots)
+      largest (last sorted)
+      _ (println "largest!" largest)
+      distances (doall (map #(taxicab (first largest) (first %)) nanobots))
+      _ (println "distances!" distances)
+      in-range (filter #(<= % (second largest)) distances)
      ]
-  (println nanobots))
+
+  (println "In range:" (count in-range)))
