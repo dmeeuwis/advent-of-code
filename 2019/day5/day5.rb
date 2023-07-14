eb61352a0c523550fe4015387bd6f2a3bddf76ff
David Meeuwis <dmeeuwis@gmail.com>
2023-07-14 15:27:49 -0700

    Day 5 solution part 2

--- 2019/day5/day5.rb
+++ 2019/day5/day5.rb
@@ -7,11 +7,14 @@ puts "Initial state: #{state}"
 OPS = { 1 => "+", 2 => "*", 3 => "I", 4 => "O", 99 => "X" }
 
 def calculate(p, input, output)
-  position =  lambda { |p, pc, inc| puts "position #{pc+inc} => #{p[p[pc+inc]]}"; p[p[pc+inc]] }
-  immediate = lambda { |p, pc, inc| puts "immediate #{pc+inc} => #{p[pc+inc]}"; p[pc+inc] }
+  #position =  lambda { |p, pc, inc| puts "position #{pc+inc} => #{p[p[pc+inc]]}"; p[p[pc+inc]] }
+  #immediate = lambda { |p, pc, inc| puts "immediate #{pc+inc} => #{p[pc+inc]}"; p[pc+inc] }
+  position =  lambda { |p, pc, inc| p[p[pc+inc]] }
+  immediate = lambda { |p, pc, inc| p[pc+inc] }
 
   pc = 0
   while true
+    chars_orig = p[pc].to_s.chars
     chars = p[pc].to_s.chars
     op = chars.pop(2).join.to_i
     param = chars.reverse.join
@@ -23,8 +26,7 @@ def calculate(p, input, output)
     param_3_toggle = param.size >= 3 && param[2] && param[2] != "0"
     param_3_mode = param_3_toggle ? immediate : position
 
-    puts "Loop: op=#{op} #{OPS[op]} param=#{param} pc=#{pc} #{param_1_toggle} #{param_2_toggle} #{param_3_toggle} "
-#   puts "State: #{p}"
+    puts "Loop: chars=#{chars_orig.join} op=#{op} #{OPS[op]} param=#{param} pc=#{pc} #{param_1_toggle} #{param_2_toggle} #{param_3_toggle} "
 
     if op == 1 # addition
       p[p[pc+3]] = param_1_mode.(p, pc, 1) + param_2_mode.(p, pc, 2)
@@ -39,9 +41,43 @@ def calculate(p, input, output)
       puts "IN #{p[p[pc+1]]} => #{pc+1}"
       pc += 2
     elsif op == 4 # output
-      output << p[p[pc+1]] # param_1_mode.(p, pc, 1) #p[pc+1]]
-      puts "OUT #{p[p[pc+1]]}" ##{param_1_mode.(p, pc, 1)}"
+      output << p[p[pc+1]]
+      puts "OUT #{p[p[pc+1]]}"
       pc += 2
+    elsif op == 5 # jump-if-true
+      if param_1_mode.(p, pc, 1) != 0
+        puts "JUMP_IF_TRUE #{param_1_mode.(p, pc, 1)}"
+        pc = param_2_mode.(p, pc, 2)
+      else
+        puts "NO_JUMP_IF_TRUE #{param_2_mode.(p, pc, 1)}"
+        pc += 3
+      end
+    elsif op == 6 # jump-if-false
+      if param_1_mode.(p, pc, 1) == 0
+        puts "JUMP_IF_FALSE #{param_1_mode.(p, pc, 1)}"
+        pc = param_2_mode.(p, pc, 2)
+      else
+        puts "NO_JUMP_IF_FALSE #{param_2_mode.(p, pc, 1)}"
+        pc += 3
+      end
+    elsif op == 7 # lt
+      if param_1_mode.(p, pc, 1) < param_2_mode.(p, pc, 2)
+        puts "LT TRUE #{pc+2} => 1    #{param_1_mode.(p, pc, 1)} < #{param_2_mode.(p, pc, 2)}"
+        p[p[pc+3]] = 1
+      else 
+        puts "LT FALSE #{pc+2} => 0   #{param_1_mode.(p, pc, 1)} < #{param_2_mode.(p, pc, 2)}"
+        p[p[pc+3]] = 0
+      end
+      pc += 4
+    elsif op == 8 # eq
+      if param_1_mode.(p, pc, 1) == param_2_mode.(p, pc, 2)
+        puts "EQ TRUE #{pc+2} => 1"
+        p[p[pc+3]] = 1
+      else 
+        puts "LT FALSE #{pc+2} => 0"
+        p[p[pc+3]] = 0
+      end
+      pc += 4
     elsif op == 99 # exit
       puts "END"
       return p
@@ -51,7 +87,13 @@ def calculate(p, input, output)
   end
 end
 
-input = [1]
+#input = [1]
+#output = []
+#end_state = calculate(state, input, output)
+#puts("Part 1: #{output}")
+#
+state = lineList[0].split(",").map { |n| n.to_i }
+input = [5]
 output = []
 end_state = calculate(state, input, output)
-puts("Part 1: #{output}")
+puts "Part 2: #{output}"
