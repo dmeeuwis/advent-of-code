c6954a9690890af4e42ca6a3a8cb62c47aa215b7
David Meeuwis <david@aha.io>
2023-12-19 22:33:48 -0800

    Day 20 part 1 solution

--- 2023-ruby/day20/day20.rb
+++ 2023-ruby/day20/day20.rb
@@ -2,17 +2,26 @@ require 'byebug'
 
 def build(line)
   line =~ /([\w%&]+) -> (.*)$/
-  name = $1
+  tname = $1
   dests = $2.split(', ')
-  if name[0] == '%'
-    FlipFlop.new name[1..], dests
-  elsif name[0] == '&'
-    Conjunction.new name[1..], dests
-  elsif name[0..8] == 'broadcast'
+  node = if tname[0] == '%'
+    name = tname[1..]
+    FlipFlop.new name, dests
+  elsif tname[0] == '&'
+    name = tname[1..]
+    Conjunction.new name, dests
+  elsif tname[0..8] == 'broadcast'
+    name = tname
     Broadcast.new name, dests
   else
     raise "Don't know what to do with line: #{line}"
   end
+
+  dests.each do |d|
+    $inputs[d].push name
+  end
+
+  node
 end
 
 class FlipFlop
@@ -25,15 +34,14 @@ class FlipFlop
   end
 
   def pulse(input, pulse)
-    puts "\t! FF #{@name} from #{input} #{pulse}"
-    return if input == 0
-    if input == 1
+    puts "#{input} #{pulse} -> #{name}"
+    return if pulse == true
+    if pulse == false
       @state = !@state
     end
 
     @dests.each do |d|
-      n = $registry[d]
-      n.pulse @name, @state
+      $queue.push [d, @name, @state]
     end
   end
 end
@@ -48,12 +56,12 @@ class Conjunction
   end
 
   def pulse(input, pulse)
-    puts "\t! CO #{@name} from #{input} #{pulse}"
+    puts "#{input} #{pulse} -> #{name}"
     @state[input] = pulse
     @dests.each do |d|
-      n = $registry[d]
-      p = !(@state.all? true)
-      n.pulse @name, p
+      all_inputs_present = $inputs[name].size == @state.size
+      p = !(all_inputs_present && @state.values.all?(true))
+      $queue.push [d, @name, p]
     end
   end
 end
@@ -67,25 +75,66 @@ class Broadcast
   end
 
   def pulse(input, pulse)
-    puts "\t! BC #{@name} from #{input} #{pulse}"
+    puts "#{input} #{pulse} -> #{name}"
     @dests.each do |d|
-      n = $registry[d]
-      n.pulse(name, pulse)
+      $queue.push [d, @name, pulse]
     end
+  end
+end
+
+class Untyped
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    puts "Untype module #{name}"
+  end
+
+  def pulse(input, pulse)
+    puts "#{input} #{pulse} -> #{name}"
+  end
+end
+
+PULSE_COUNTS = { false => 0, true => 0 }
+
+def work
+  while !($queue.empty?)
+    work = $queue.pop
+    node = $registry[work[0]]
+    if node.nil?
+      node = Untyped.new work[0] 
+    end
+
+    PULSE_COUNTS[work[2]] += 1
+    node.pulse(work[1], work[2])
   end
 end
 
 f = File.open(ARGV[0])
 lines = f.readlines.map { |l| l.chomp }
 
+$queue = Queue.new
 $registry = {}
+$inputs = Hash.new { |h, k| h[k] = [] }
 lines.each do |l|
   node = build l
   $registry[node.name] = node
 end
+puts "Inputs hash: #{$inputs}"
+
 
 puts
 puts
-$registry['broadcaster'].pulse(nil, false)
+puts "Ready to start work!"
 puts
-byebug
+puts
+PRESSES = 1000
+
+PRESSES.times do
+  PULSE_COUNTS[false] += 1
+  $registry['broadcaster'].pulse(nil, false)
+  work
+  puts 
+  puts 
+end
+
+puts "Low pulses: #{PULSE_COUNTS[false]} x high pulses #{PULSE_COUNTS[true]} = #{PULSE_COUNTS[false] * PULSE_COUNTS[true]}"
