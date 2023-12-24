25981a11cc5d9aaf9636f3b59b0be47cb5f44196
David Meeuwis <david@aha.io>
2023-12-23 16:01:19 -0800

    Day 23 part 2

--- 2023-ruby/day23/day23p2.rb
+++ 2023-ruby/day23/day23p2.rb
@@ -18,28 +18,35 @@ DIRS = [ [-1, 0], [0, 1], [1, 0], [0, -1] ]
 START = [0, 1]
 
 $lengths = []
-def walk(grid, pos, seen, path_length = 0)
-  seen.add pos
+stack = []
+stack.push [START, Set.new, 0]
+def walk(grid, stack)
+  while !stack.empty?
+    puts stack.size if stack.size % 100_000 == 0
+    pos, seen, path_length = stack.pop
+    seen.add pos
 
-  if pos == [ grid.size-1, grid[0].size-2]
-    puts "Found the end! path-length #{path_length}"
-    $lengths.push path_length
-    return path_length
-  end
+    if pos == [ grid.size-1, grid[0].size-2]
+      puts "Found the end! path-length #{path_length}"
+      $lengths.push path_length
+    end
 
-  DIRS.each do |dir|
-    n = [pos[0] + dir[0], pos[1] + dir[1]]
-    next if n[0] < 0
-    next if seen.include? n
+    DIRS.each do |dir|
+      n = [pos[0] + dir[0], pos[1] + dir[1]]
+      next if n[0] < 0
+      next if n[0] >= grid.size
+      next if seen.include? n
+      #puts n
 
-    ng = grid[n[0]][n[1]] 
-    next if ng == '#'
-    #puts "walk: #{pos} #{path_length} => #{n} #{ng}"
-    if ng == '.' || ng = '>' || ng == '<' || ng == '^' || ng == 'v'
-      walk grid, n, seen.clone, path_length + 1
+      ng = grid[n[0]][n[1]] 
+      next if ng == '#'
+      #puts "walk: #{pos} #{path_length} => #{n} #{ng}"
+      if ng == '.' || ng = '>' || ng == '<' || ng == '^' || ng == 'v'
+        stack.push [n, seen.clone, path_length + 1]
+      end
     end
   end
 end
 
-walk(grid, START, Set.new)
+walk(grid, stack)
 puts "Longest walk: #{$lengths.max}"
