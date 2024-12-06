5a6cfaa94a8bd0f3ee9b46ff822746ccf7402037
David Meeuwis <david@aha.io>
2024-12-05 23:09:10 -0800

    Day 5 part 1 working

--- 2024-ruby/day_05/day5.rb
+++ 2024-ruby/day_05/day5.rb
@@ -3,12 +3,10 @@ require 'byebug'
 lines = File.readlines ARGV[0]
 split_index = lines.index("\n")
 
-before_rules = {}
-after_rules = {}
+before_rules = Hash.new { |h, k| h[k] = [] }
 rules = lines[0..split_index-1].map(&:strip).map do |rule|
    parts = rule.split("|")
-   before_rules[parts[1].to_i] = parts[0].to_i
-   after_rules[parts[0].to_i] = parts[1].to_i
+   before_rules[parts[0].to_i].push parts[1].to_i
 end
 
 update_lines = lines[split_index+1..-1].map(&:strip)
@@ -17,19 +15,12 @@ updates = update_lines.map do |update|
 end
 
 
-def check_update(update, before_rules, after_rules)
+def check_update(update, before_rules)
   update.each_with_index do |page, index|
-    before = update[0...index]
     after = update[index+1...]
 
-    before.each do |b|
-      if after_rules[page] == b
-        return false
-      end
-    end
-
     after.each do |a|
-      if before_rules[page] == a
+      if !before_rules[page].include? a
         return false
       end
     end
@@ -40,7 +31,7 @@ end
 
 sum = 0
 updates.each do |update|
-  res = check_update(update, before_rules, after_rules)
+  res = check_update(update, before_rules)
   if res
     # get the middle element
     raise "Invalid update" if update.size.even?
