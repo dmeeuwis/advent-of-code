9f468f13a1dee5572b0ebef07eac82a39ee7fe18
David Meeuwis <david@aha.io>
2024-12-06 22:10:03 -0800

    Day 7 part 1

---
+++ 2024-ruby/day_07/day7.rb
@@ -0,0 +1,58 @@
+require 'byebug'
+
+$operators = ['*', '+'].freeze
+lines = File.readlines ARGV[0]
+
+tests = lines.map do |line|
+  r = {}
+  m = line.match(/(\d*): (.*)/)
+  {
+    test_value: m[1].to_i,
+     equation: m[2].split(' ').map(&:to_i)
+  }
+end
+
+def build_str(equation, eqn = '', col = [])
+  val = equation.shift
+  eqn += val.to_s
+  eqn += ' '
+
+  if equation.empty?
+    col.push eqn
+    return
+  end
+
+  $operators.each do |op|
+    build_str(equation.dup, eqn + op + ' ', col)
+  end
+end
+
+def eval_eqn(equation)
+  parts = equation.split(' ')
+  value = 0
+  operand1 = parts.shift
+  while !parts.empty?
+    operator = parts.shift
+    operand2 = parts.shift
+
+    operand1 = eval "#{operand1} #{operator} #{operand2}"
+  end
+  operand1
+end
+
+passing_tests = Set.new
+tests.each do |test|
+  puts test
+  col = []
+  build_str(test[:equation], '', col)
+  col.each do |c|
+    puts "\t#{c}"
+    res = eval_eqn(c)
+    if test[:test_value] == res
+      puts "\t\tFound match: #{c} = #{res}"
+      passing_tests.add test[:test_value]
+    end
+  end
+end
+
+puts "Count is #{passing_tests.to_a.sum}"
